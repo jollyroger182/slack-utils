6ddf588b2cc0ea44457e89b9f0be4345e509d29e
jollyroger182 <hello@jollyy.dev>
2026-06-26 22:53:19 -0400

    id

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import { App } from 'slack.ts'
+
+const { BOT_TOKEN, SIGNING_SECRET, COMMAND_PREFIX = '', PORT = '3000' } = process.env
+if (!BOT_TOKEN || !SIGNING_SECRET) {
+	throw new Error('BOT_TOKEN or SIGNING_SECRET environment variable not set')
+}
+
+const app = new App({
+	token: BOT_TOKEN,
+	receiver: { type: 'http', port: Number(PORT), signingSecret: SIGNING_SECRET },
+})
+
+app.on(`/${COMMAND_PREFIX}id`, async (event) => {
+	const text = event.text.replace(/^\s*<(?:\!subteam\^|@|#)([0-9A-Z]+)(?:\|.*)?>\s*$/, (_, x) => x)
+	await event.respond.message({ ephemeral: true, text })
+})
+
+await app.start()
